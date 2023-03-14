9bc90b8eaeef9469bbe8e8a3ec7a95514977399a
Santiago Rios <santiagorios94@yahoo.com>
2023-03-14 18:57:17 -0300

    Arreglo ej5

--- Ej5.java
+++ Ej5.java
@@ -14,15 +14,15 @@ import java.util.Scanner;
 public class Ej5 {
 
     public static void main(String[] args) {
-       
-        Scanner leer = new Scanner(System.in) ;
+
+        Scanner leer = new Scanner(System.in);
         System.out.println("Ingrese un numero");
         int numero = leer.nextInt();
-        
-        System.out.println("Doble = " + numero*2);
-        System.out.println("Triple = " + numero*3);
+
+        System.out.println("Doble = " + numero * 2);
+        System.out.println("Triple = " + numero * 3);
         System.out.println("Raiz cuadrada = " + Math.sqrt(numero));
-        
+
     }
-    
+
 }
